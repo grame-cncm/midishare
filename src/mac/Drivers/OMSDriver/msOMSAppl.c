66cdc3cf85e8945dcc7fb0ae6abe78a7d9d3d959
fober <fober>
2000-03-23 09:38:14 +0000

    SetupWindow restored for the application target

--- src/mac/Drivers/OMSDriver/msOMSAppl.c
+++ src/mac/Drivers/OMSDriver/msOMSAppl.c
@@ -153,13 +153,6 @@ static void SetUpMenus()
 /* -----------------------------------------------------------------------------*/
 static void ShowAbout()
 {										
-	DialogPtr	dlog;					
-	short		Hit; 							
- 	dlog = GetNewDialog (dialogID, nil, (WindowPtr) -1);
-	if (dlog) {
-		ModalDialog(nil, &Hit);
-		DisposeDialog(dlog);
-	}
 }
 			
 /* -----------------------------------------------------------------------------*/
@@ -262,7 +255,7 @@ static void Initialize()
 	if (!SetUpMidi()) 			AlertUser ("\pMidiShare initialization failed");
 	foreGround = true;
 	SetUpMenus();
-//	SetUpWindows();
+	SetUpWindows();
 }
 
 /*======================================== main loop =======================================*/
