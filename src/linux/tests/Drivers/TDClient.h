06f2638e464bcbc2210826331f9b38dc5444ef2c
fober <fober>
2000-04-25 07:35:35 +0000

    connection test entry point added

--- src/linux/tests/Drivers/TDClient.h
+++ src/linux/tests/Drivers/TDClient.h
@@ -17,6 +17,7 @@ static inline void FEnvironment()		{ IOCTL(kEnvironment); }
 static inline void FOpenClose()			{ IOCTL(kOpenClose); }
 static inline void FWakeupSleep()		{ IOCTL(kWakeupSleep); }
 static inline void FInfos()			{ IOCTL(kInfos); }
+static inline void FConnections()		{ IOCTL(kConnections); }
 static inline void FSlots()			{ IOCTL(kSlots); }
 static inline void FSendingAndReceiving()	{ IOCTL(kSendingAndReceiving); }
 #endif
