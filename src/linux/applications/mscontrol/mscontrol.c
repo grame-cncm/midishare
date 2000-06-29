0cd84c7199f48ba46a7454d159677f1aaa1a2029
letz <letz>
2000-06-29 13:58:08 +0000

    remove detete_event signal, clean up

--- src/linux/applications/mscontrol/mscontrol.c
+++ src/linux/applications/mscontrol/mscontrol.c
@@ -18,9 +18,8 @@
 
 static short 		gRefNum = -1;		// msControl MidiShare reference number
 
-GtkObject 	*pitch, *velocity, *duration, 
-			*channel, *port, 
-			*prog, *vol, *mod, *pan;
+GtkObject 	*gPitch, *gVelocity, *gDuration, 
+		*gChannel, *gPort, *gProg, *gVol, *gMod, *gPan;
 
 
 /****************************************************************************
@@ -74,11 +73,11 @@ void note_action (GtkAdjustment* adj, GtkWidget* data)
 {
 	MidiEvPtr e = MidiNewEv(typeNote);
 	
-	Pitch(e) = GTK_ADJUSTMENT(pitch)->value + 0.5; 
-	Vel(e) = GTK_ADJUSTMENT(velocity)->value + 0.5; 
-	Dur(e) = GTK_ADJUSTMENT(duration)->value + 0.5;
-	Chan(e) = GTK_ADJUSTMENT(channel)->value + 0.5; 
-	Port(e) = GTK_ADJUSTMENT(port)->value + 0.5; 
+	Pitch(e) = GTK_ADJUSTMENT(gPitch)->value + 0.5; 
+	Vel(e) = GTK_ADJUSTMENT(gVelocity)->value + 0.5; 
+	Dur(e) = GTK_ADJUSTMENT(gDuration)->value + 0.5;
+	Chan(e) = GTK_ADJUSTMENT(gChannel)->value + 0.5; 
+	Port(e) = GTK_ADJUSTMENT(gPort)->value + 0.5; 
 	
 	MidiSendIm(gRefNum, e);
 }
@@ -87,9 +86,9 @@ void prog_action (GtkAdjustment* adj, GtkWidget* data)
 {
 	MidiEvPtr e = MidiNewEv(typeProgChange);
 	
-	Pitch(e) = GTK_ADJUSTMENT(prog)->value + 0.5; 
-	Chan(e) = GTK_ADJUSTMENT(channel)->value + 0.5; 
-	Port(e) = GTK_ADJUSTMENT(port)->value + 0.5; 
+	Pitch(e) = GTK_ADJUSTMENT(gProg)->value + 0.5; 
+	Chan(e) = GTK_ADJUSTMENT(gChannel)->value + 0.5; 
+	Port(e) = GTK_ADJUSTMENT(gPort)->value + 0.5; 
 	
 	MidiSendIm(gRefNum, e);
 }
@@ -99,9 +98,9 @@ void ctrl_action (GtkAdjustment* adj, long ctrlnumber)
 	MidiEvPtr e = MidiNewEv(typeCtrlChange);
 	
 	Pitch(e) = ctrlnumber; 
-	Vel(e) = GTK_ADJUSTMENT(vol)->value + 0.5; 
-	Chan(e) = GTK_ADJUSTMENT(channel)->value + 0.5; 
-	Port(e) = GTK_ADJUSTMENT(port)->value + 0.5; 
+	Vel(e) = GTK_ADJUSTMENT(gVol)->value + 0.5; 
+	Chan(e) = GTK_ADJUSTMENT(gChannel)->value + 0.5; 
+	Port(e) = GTK_ADJUSTMENT(gPort)->value + 0.5; 
 	
 	MidiSendIm(gRefNum, e);
 }
@@ -125,16 +124,16 @@ int main(int argc, char *argv[] )
 	MidiConnect(0,gRefNum,1);
 	
 	// Adjustments description  
-	pitch 	= gtk_adjustment_new (60.0, 1.0, 128.0, 1.0, 8.0, 1.0);
-	velocity= gtk_adjustment_new (64.0, 1.0, 128.0, 1.0, 8.0, 1.0);
-	duration= gtk_adjustment_new (250.0, 50.0, 2000.0, 1.0, 50.0, 1.0);
-	channel = gtk_adjustment_new (0.0, 0.0, 16.0, 1.0, 1.0, 1.0);
-	port 	= gtk_adjustment_new (0.0, 0.0, 128.0, 1.0, 1.0, 1.0);
+	gPitch 	= gtk_adjustment_new (60.0, 1.0, 128.0, 1.0, 8.0, 1.0);
+	gVelocity= gtk_adjustment_new (64.0, 1.0, 128.0, 1.0, 8.0, 1.0);
+	gDuration= gtk_adjustment_new (250.0, 50.0, 2000.0, 1.0, 50.0, 1.0);
+	gChannel = gtk_adjustment_new (0.0, 0.0, 16.0, 1.0, 1.0, 1.0);
+	gPort 	= gtk_adjustment_new (0.0, 0.0, 128.0, 1.0, 1.0, 1.0);
 
-	prog	= gtk_adjustment_new (0.0, 0.0, 128.0, 1.0, 8.0, 1.0);
-	vol		= gtk_adjustment_new (100.0, 0.0, 128.0, 1.0, 8.0, 1.0);
-	mod		= gtk_adjustment_new (0.0, 0.0, 128.0, 1.0, 8.0, 1.0);
-	pan		= gtk_adjustment_new (64.0, 0.0, 128.0, 1.0, 8.0, 1.0);
+	gProg	= gtk_adjustment_new (0.0, 0.0, 128.0, 1.0, 8.0, 1.0);
+	gVol	= gtk_adjustment_new (100.0, 0.0, 128.0, 1.0, 8.0, 1.0);
+	gMod	= gtk_adjustment_new (0.0, 0.0, 128.0, 1.0, 8.0, 1.0);
+	gPan	= gtk_adjustment_new (64.0, 0.0, 128.0, 1.0, 8.0, 1.0);
 
 
 	// Window definition
@@ -145,43 +144,39 @@ int main(int argc, char *argv[] )
 	vbox = gtk_vbox_new (FALSE, 10);
 	
 	// Add controllers
-	add_control(vbox, "Pitch", pitch);
-	add_control(vbox, "Vel", velocity);
-	add_control(vbox, "Dur", duration);
+	add_control(vbox, "Pitch", gPitch);
+	add_control(vbox, "Vel", gVelocity);
+	add_control(vbox, "Dur", gDuration);
 	
 	add(vbox, gtk_hseparator_new());
 
-	add_control(vbox, "Prog", prog);
-	add_control(vbox, "Vol", vol);
-	add_control(vbox, "Mod", mod);
-	add_control(vbox, "Pan", pan);
+	add_control(vbox, "Prog", gProg);
+	add_control(vbox, "Vol", gVol);
+	add_control(vbox, "Mod", gMod);
+	add_control(vbox, "Pan", gPan);
 	
 	add(vbox, gtk_hseparator_new());
 
-	add_control(vbox, "Chan", channel);
-	add_control(vbox, "Port", port);
+	add_control(vbox, "Chan", gChannel);
+	add_control(vbox, "Port", gPort);
 	
 	
 	gtk_container_add(GTK_CONTAINER(window), vbox);	
 	gtk_widget_show_all (window);
 	
 	// Connexion of signals for value adjustement
-	gtk_signal_connect( pitch, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
-	gtk_signal_connect( velocity, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
-	gtk_signal_connect( duration, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
-	gtk_signal_connect( channel, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
-	gtk_signal_connect( port, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
+	gtk_signal_connect( gPitch, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
+	gtk_signal_connect( gVelocity, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
+	gtk_signal_connect( gDuration, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
+	gtk_signal_connect( gChannel, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
+	gtk_signal_connect( gPort, "value_changed", GTK_SIGNAL_FUNC(note_action), NULL);
 
-	gtk_signal_connect( prog, "value_changed", GTK_SIGNAL_FUNC(prog_action), NULL);
-	gtk_signal_connect( vol, "value_changed", GTK_SIGNAL_FUNC(ctrl_action), (void*)7L);
-	gtk_signal_connect( mod, "value_changed", GTK_SIGNAL_FUNC(ctrl_action), (void*)1L);
-	gtk_signal_connect( pan, "value_changed", GTK_SIGNAL_FUNC(ctrl_action), (void*)10L);
+	gtk_signal_connect( gProg, "value_changed", GTK_SIGNAL_FUNC(prog_action), NULL);
+	gtk_signal_connect( gVol, "value_changed", GTK_SIGNAL_FUNC(ctrl_action), (void*)7L);
+	gtk_signal_connect( gMod, "value_changed", GTK_SIGNAL_FUNC(ctrl_action), (void*)1L);
+	gtk_signal_connect( gPan, "value_changed", GTK_SIGNAL_FUNC(ctrl_action), (void*)10L);
 
 	// Connexion of signals
-	gtk_signal_connect(
-			GTK_OBJECT(window), "delete_event", 
-			GTK_SIGNAL_FUNC(my_delete_action), NULL
-	);
 	
 	gtk_signal_connect(
 			GTK_OBJECT(window), "destroy", 
