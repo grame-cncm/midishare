11ad7c2bfbbfcbe2f1e41de9ba6b7b5e222439ae
letz <letz>
2000-10-06 10:55:19 +0000

    Remove obsolete filters macros

--- src/linux/Include/MidiShare.h
+++ src/linux/Include/MidiShare.h
@@ -435,28 +435,6 @@ enum{	MIDIOpenAppl=1,
 	#define FirstEv(e)	( (e)->first )
 	#define LastEv(e)	( (e)->last )
 
-/******************************************************************************
-* 							FILTER MACROS							
-*------------------------------------------------------------------------------
-* somes macros to set and reset filter's bits. 
-*******************************************************************************/
-
-#ifdef __cplusplus
-inline void 	AcceptBit( char* a, Byte n) 		{ (a)[(n)>>3] |=  (1<<((n)&7)); }
-inline void 	RejectBit( char* a, Byte n) 		{ (a)[(n)>>3] &= ~(1<<((n)&7)); }
-inline void 	InvertBit( char* a, Byte n) 		{ (a)[(n)>>3] ^=  (1<<((n)&7)); }
-
-inline Boolean 	IsAcceptedBit( char* a, Byte n) 	{ return (a)[(n)>>3] & (1<<((n)&7)); }
-
-#else
-
-	#define AcceptBit(a,n) 			( ((char*) (a))[(n)>>3] |=   (1<<((n)&7)) )
-	#define RejectBit(a,n) 			( ((char*) (a))[(n)>>3] &=  ~(1<<((n)&7)) )
-	#define InvertBit(a,n) 			( ((char*) (a))[(n)>>3] ^=   (1<<((n)&7)) )
-
-	#define IsAcceptedBit(a,n) 		( ((char*) (a))[(n)>>3]  &   (1<<((n)&7)) )
-#endif
-
 
 /*******************************************************************************
 * 						MidiShare API									
