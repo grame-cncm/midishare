c58922571b472192c1984f884017081c4871bf02
fober <fober>
2000-11-08 11:29:15 +0000

    cplusplus extern C declaration

--- src/lib/FilterUtils.h
+++ src/lib/FilterUtils.h
@@ -36,10 +36,12 @@ These macros can be used to access bit fields, the filter management functions
  *******************************************************************************/
                                                                                                                                                                                      
  #ifdef __cplusplus
- 	inline void     AcceptBit( char* a, Byte n)       { (a)[(n)>>3] |=  (1<<((n)&7)); }
+ extern "C" {
+	inline void     AcceptBit( char* a, Byte n)       { (a)[(n)>>3] |=  (1<<((n)&7)); }
  	inline void     RejectBit( char* a, Byte n)       { (a)[(n)>>3] &= ~(1<<((n)&7)); }
  	inline void     InvertBit( char* a, Byte n)       { (a)[(n)>>3] ^=  (1<<((n)&7)); }
  	inline Boolean  IsAcceptedBit( char* a, Byte n)   { return (a)[(n)>>3] & (1<<((n)&7)); }
+ }
  #else
      	#define AcceptBit(a,n)         ( ((char*) (a))[(n)>>3] |=   (1<<((n)&7)) )
      	#define RejectBit(a,n)         ( ((char*) (a))[(n)>>3] &=  ~(1<<((n)&7)) )
