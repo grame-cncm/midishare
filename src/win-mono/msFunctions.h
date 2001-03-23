dec5546d72734bc194187192bdafb6d35947dd73
fober <fober>
2001-03-23 14:42:31 +0000

    copyright added

--- src/win-mono/msFunctions.h
+++ src/win-mono/msFunctions.h
@@ -1,6 +1,6 @@
 /*
 
-  Copyright © Grame 2001
+  Copyright © Grame, Sony CSL-Paris 2001
 
   This library is free software; you can redistribute it and modify it under 
   the terms of the GNU Library General Public License as published by the 
