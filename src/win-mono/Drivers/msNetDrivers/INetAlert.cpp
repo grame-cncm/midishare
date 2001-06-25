09c103b5a7d3feccea90312d91a8bdedfd11fc5a
fober <fober>
2001-06-25 11:32:13 +0000

    log function implementation

--- src/win-mono/Drivers/msNetDrivers/INetAlert.cpp
+++ src/win-mono/Drivers/msNetDrivers/INetAlert.cpp
@@ -20,14 +20,62 @@
 
 */
 
-
-#include <stdio.h>
-#include <string.h>
+#include <time.h>
+#include <windows.h>
 
 #include "INetAlert.h"
 
 extern Boolean doneFlag;
-void	log (char *msg);
+
+#ifdef WANDriver
+static char * ErrFile = "\\msWANDriver.log";
+#else
+static char * ErrFile = "\\msLANDriver.log";
+#endif
+
+//_________________________________________________________
+char *dateString ()
+{
+	char *ptr;
+	time_t t;
+	time (&t);
+	ptr = asctime (localtime (&t));
+	if (ptr) {
+		short n = strlen (ptr);
+		if (n) ptr[n-1] = 0;
+	}
+	return ptr;
+}
+
+//_________________________________________________________
+static char * ErrFilePath ()
+{
+	static char buff[MAX_PATH ];
+	UINT len = GetWindowsDirectory (buff, MAX_PATH);
+	if (len && (len + strlen(ErrFile) < MAX_PATH)) {
+		lstrcat (buff, ErrFile);
+		return buff;
+	}
+	return 0;
+}
+
+//_________________________________________________________
+void log (char * msg)
+{
+	char * errFile = ErrFilePath ();
+	char buff[1024];
+	wsprintf (buff, "%s\n", msg);
+	if (errFile) {
+		HANDLE h = CreateFile (errFile, GENERIC_WRITE, FILE_SHARE_READ,
+			NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (h != INVALID_HANDLE_VALUE) {
+			DWORD written;
+			SetFilePointer(h, 0, 0, FILE_END);
+			WriteFile(h, buff, strlen(buff), &written, NULL);
+			CloseHandle (h);
+		}
+	}
+}
 
 //_________________________________________________________________________________
 void INetAlert::Report (const ErrString what, const ErrString obj, 
@@ -41,7 +89,8 @@ void INetAlert::Report (const ErrString what, const ErrString obj,
 	const ErrString reason, const ErrString infos, void *proc)
 {
 	char msg[512];
-	sprintf (msg, "%s: %s, %s %s\n", what, obj, reason, infos);
+	wsprintf (msg, "%s: %s: %s, %s %s\n", dateString(), 
+			what, obj, reason, infos);
 	log (msg);
 }
 
