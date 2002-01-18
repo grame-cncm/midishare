5c0c05ae09a460cdc5019212a4de912bb4db5911
letz <letz>
2002-01-18 22:21:22 +0000

    Cleanup

--- src/macosx-mono/macosx.h
+++ src/macosx-mono/macosx.h
@@ -3,22 +3,4 @@
 #define __MACOSX__
 #define DARWIN
 
-/* pour iiwusyth */
 
-#define MACOS
-#define MACINTOSH
-
-#define HAVE_STRING_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STDIO_H 1
-#define HAVE_MATH_H 1
-#define HAVE_STDARG_H 1
-
-/* define to support the macintosh sound manager */
-/* #define SNDMAN_SUPPORT 1 */
-
-/* define to support the MidiShare driver */
-#define HAVE_LIBMIDISHARE 1
-#define MIDISHARE_DRIVER 1
-#define PORTAUDIO_SUPPORT 1
-#define MidiSharePPC_68k
