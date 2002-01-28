2b354b6f159724c01abee44df8c6fe2dbc7356ef
letz <letz>
2002-01-28 23:36:12 +0000

    Change the location of temporary files created in write_private_profile_string

--- src/lib/profport.c
+++ src/lib/profport.c
@@ -1,13 +1,23 @@
+/*
+History : 
+01-28-02 : Change the location of temporary files created in write_private_profile_string
+		   now done in MidiShareDirectory.
+*/
+
 
 /***** Routines to read profile strings --  by Joseph J. Graf ******/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
 /* #include <ctype.h> */
 
-static int isdigit (char c) {
+extern char* MidiShareDirectory;
+
+static int isdigit (char c) 
+{
 	static char digits[10]={'0','1','2','3','4','5','6','7','8','9'};
 	int i;
 	for (i = 0 ; i < 10; i++) if (digits[i]==c) return 1;
@@ -27,7 +37,7 @@ static int read_line(FILE *fp, char *bp);
 
 static int read_line(FILE *fp, char *bp)
 {  
-	char c = '\0';
+    char c = '\0';
     int i = 0;
     /* Read one line from the source file */
     while( (c = getc(fp)) != '\n' )
@@ -50,8 +60,8 @@ static int read_line(FILE *fp, char *bp)
 int get_private_profile_int(char *section,
     char *entry, int def, char *file_name)
 {   
-	FILE *fp = fopen(file_name,"r");
-   char buff[MAX_LINE_LENGTH];
+    FILE *fp = fopen(file_name,"r");
+    char buff[MAX_LINE_LENGTH];
     char *ep;
     char t_section[MAX_LINE_LENGTH];
     char value[6];
@@ -101,14 +111,14 @@ int get_private_profile_int(char *section,
 int get_private_profile_string(char *section, char *entry, char *def,
     char *buffer, int buffer_len, char *file_name)
 {   
-	FILE *fp = fopen(file_name,"r");
+    FILE *fp = fopen(file_name,"r");
     char buff[MAX_LINE_LENGTH];
     char *ep;
     char t_section[MAX_LINE_LENGTH];
     int len = strlen(entry);
     
     if( !fp ) return(0);
-    sprintf(t_section,"[%s]",section);    /* Format the section name */
+     sprintf(t_section,"[%s]",section);    /* Format the section name */
     /*  Move through file 1 line at a time until a section is matched or EOF */
     do
     {   if( !read_line(fp,buff) )
@@ -151,68 +161,84 @@ int write_private_profile_string(char *section,
     char *entry, char *buffer, char *file_name)
 
 {   FILE *rfp, *wfp;
-    char tmp_name[15];
+    char* tmp_name;
     char buff[MAX_LINE_LENGTH];
     char t_section[MAX_LINE_LENGTH];
     int len = strlen(entry);
-    tmpnam(tmp_name); /* Get a temporary file name to copy to */
+    
+    //Does not work on MacOSX
+    //char tmp_name[15];
+    //tmpnam(tmp_name); /* Get a temporary file name to copy to */
+    
+    tmp_name = tempnam(MidiShareDirectory,"");
     sprintf(t_section,"[%s]",section);/* Format the section name */
+    
     if( !(rfp = fopen(file_name,"r")) )  /* If the .ini file doesn't exist */
     {   if( !(wfp = fopen(file_name,"w")) ) /*  then make one */
-        {   return(0);   }
+        {   
+            return(0);   
+        }
         fprintf(wfp,"%s\n",t_section);
         fprintf(wfp,"%s=%s\n",entry,buffer);
         fclose(wfp);
         return(1);
     }
     if( !(wfp = fopen(tmp_name,"w")) )
-    {   fclose(rfp);
+    {   
+        fflush(rfp);
+        fclose(rfp);
         return(0);
     }
     /* Move through the file one line at a time until a section is
      * matched or until EOF. Copy to temp file as it is read. */
     do
     {   if( !read_line(rfp,buff) )
-        {   /* Failed to find section, so add one to the end */
+        {   
+            /* Failed to find section, so add one to the end */
             fprintf(wfp,"\n%s\n",t_section);
             fprintf(wfp,"%s=%s\n",entry,buffer);
             /* Clean up and rename */
             fclose(rfp);
             fclose(wfp);
-            unlink(file_name);  
+            unlink(file_name);
             rename(tmp_name,file_name);
             return(1);
         }
         fprintf(wfp,"%s\n",buff);
-    } while( strcmp(buff,t_section) );
+     } while( strcmp(buff,t_section) );
     /* Now that the section has been found, find the entry. Stop searching
      * upon leaving the section's area. Copy the file as it is read
      * and create an entry if one is not found.  */
     while( 1 )
-    {   if( !read_line(rfp,buff) )
+    {   
+        if( !read_line(rfp,buff) )
         {   /* EOF without an entry so make one */
             fprintf(wfp,"%s=%s\n",entry,buffer);
             /* Clean up and rename */
             fclose(rfp);
             fclose(wfp);
-            unlink(file_name); 
+            unlink(file_name);
             rename(tmp_name,file_name);
             return(1);
 
         }
         if( !strncmp(buff,entry,len) || buff[0] == '\0' )
             break;
+            
         fprintf(wfp,"%s\n",buff);
     }
+    
     if( buff[0] == '\0' )
-    {   fprintf(wfp,"%s=%s\n",entry,buffer);
+    {   
+       fprintf(wfp,"%s=%s\n",entry,buffer);
         do
         {
             fprintf(wfp,"%s\n",buff);
         } while( read_line(rfp,buff) );
     }
     else
-    {   fprintf(wfp,"%s=%s\n",entry,buffer);
+    {   
+        fprintf(wfp,"%s=%s\n",entry,buffer);
         while( read_line(rfp,buff) )
         {
              fprintf(wfp,"%s\n",buff);
@@ -221,7 +247,7 @@ int write_private_profile_string(char *section,
     /* Clean up and rename */
     fclose(wfp);
     fclose(rfp);
-    unlink(file_name);  
+    unlink(file_name);
     rename(tmp_name,file_name);
     return(1);
 }
