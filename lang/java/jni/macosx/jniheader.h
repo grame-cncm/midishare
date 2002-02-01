84cced1b29c9e31ebb3cfe4697dc95ef04d54fd3
letz <letz>
2002-02-01 17:35:30 +0000

    Init

---
+++ lang/java/jni/macosx/jniheader.h
@@ -0,0 +1,13 @@
+/*#include <MacHeaders.h> */
+
+
+#define __Macintosh__
+#define FAR 
+#define NEAR
+#define EXPORT 
+#define MFAPI
+#define MSALARMAPI
+/*#define MSALARMAPI pascal */
+#include <Carbon/Carbon.h>
+
+#define __DEBUG__ 0
