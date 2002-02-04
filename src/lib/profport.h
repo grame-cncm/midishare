d2cd3e06d98e165322c7149277efe68083dae849
fober <fober>
2002-02-04 18:22:44 +0000

    extern C declaration

--- src/lib/profport.h
+++ src/lib/profport.h
@@ -4,8 +4,21 @@
  Header file containing prototypes and compile-time configuration.
 ******************************************************************************/
 
-#define MAX_LINE_LENGTH    80
+#ifndef __profport__
+#define __profport__
+
+#define MAX_LINE_LENGTH    1024
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int get_private_profile_int(char *, char *, int, char *);
 int get_private_profile_string(char *, char *, char *, char *, int, char *);
 int write_private_profile_string(char *, char *, char *, char *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
