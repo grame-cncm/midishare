d9e51ac7da5ab7bde87ef12d1de51f8628d0fd94
fober <fober>
2002-09-13 16:32:20 +0000

    first import

---
+++ src/osglue/msThreads.cpp
@@ -0,0 +1,49 @@
+/*
+  Copyright © Grame 2002
+
+  This library is free software; you can redistribute it and modify it under 
+  the terms of the GNU Library General Public License as published by the 
+  Free Software Foundation version 2 of the License, or any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License 
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Grame Research Laboratory, 9, rue du Garet 69001 Lyon - France
+  grame@grame.fr
+  
+  modifications history:
+ 
+*/
+
+#include "TLog.h"
+#include "msThreads.h"
+#include "msServerInit.h"
+
+extern TLog gLog;
+
+//___________________________________________________________________
+msThreadPtr msThreadCreate (ThreadProcPtr proc, void * arg, int priority)
+{
+	TThreads * thread = new TThreads (&gLog);
+	if (thread) {
+		if (!thread->Create (proc, arg, priority)) {
+			delete thread;
+			thread = 0;
+		}
+	}
+	else LogWrite ("Cannot create a new TThreads object");
+	return (msThreadPtr)thread;
+}
+
+//___________________________________________________________________
+void msThreadDelete (msThreadPtr thread)
+{
+	TThreads * threadobj = (TThreads *)thread;
+	if (threadobj) delete threadobj;
+}
---
+++ src/osglue/msThreads.h
@@ -0,0 +1,56 @@
+/*
+  Copyright © Grame 2002
+
+  This library is free software; you can redistribute it and modify it under 
+  the terms of the GNU Library General Public License as published by the 
+  Free Software Foundation version 2 of the License, or any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License 
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Grame Research Laboratory, 9, rue du Garet 69001 Lyon - France
+  grame@grame.fr
+  
+  modifications history:
+ 
+*/
+
+#ifndef __msThreads__
+#define __msThreads__
+
+
+#ifdef WIN32
+	typedef LPTHREAD_START_ROUTINE ThreadProcPtr;
+#else
+	typedef void * ( * ThreadProcPtr) (void * ptr);
+#endif
+
+typedef void * msThreadPtr;
+
+
+enum { 
+	kNormalPriority=0, 
+	kClientHighPriority=49, 
+	kClientRTPriority=90, 
+	kServerHighPriority=50,
+	kServerRTPriority=99 
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+msThreadPtr msThreadCreate (ThreadProcPtr proc, void * arg, int priority);
+void        msThreadDelete (msThreadPtr thread);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
