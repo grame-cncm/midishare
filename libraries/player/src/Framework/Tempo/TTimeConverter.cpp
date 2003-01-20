32bae1d2a62e22b926c014170f244ba731ba54be
letz <letz>
2003-01-20 13:00:44 +0000

    Check TimeSign parameters validity

--- libraries/player/src/Framework/Tempo/TTimeConverter.cpp
+++ libraries/player/src/Framework/Tempo/TTimeConverter.cpp
@@ -17,7 +17,7 @@
 //
 
 #include "TTimeConverter.h"
-
+#include <stdio.h>
 
 /*--------------------------------------------------------------------------*/
 
@@ -46,52 +46,46 @@ void TTimeConverter::Init ()
 
 /*--------------------------------------------------------------------------*/
 
-void TTimeConverter::Update (MidiEvPtr ts) 
+void TTimeConverter::Update (MidiEvPtr ts)
 {
-	if ((TSNum(ts) != fNumerator)   		// Update values only if the TimeSign has changed
-		|| (TSDenom(ts) != fDenominator) 
-		|| (TSClocks(ts) != fNClocks)
-		|| (TS32nd(ts) != fN32nd)) {
- 
- 		fLast_pos = ConvertTickToBBU (Date(ts));
-	
-		fInt.fBeat_per_bar =  TSNum(ts);
-		fInt.fTick_per_beat = CalcBeat (fInt.fTicks_per_quarter, TSDenom(ts) - 2);
-		fInt.fTick_per_bar = fInt.fBeat_per_bar * fInt.fTick_per_beat;
-	
-		fNumerator = TSNum(ts);
-		fDenominator = TSDenom(ts);
-		fNClocks = TSClocks(ts);
-		fN32nd = TS32nd(ts);
-	
-		fLast_timesign = Date(ts);
-	}
- }
- 
- /*--------------------------------------------------------------------------*/
+	Update(Date(ts), TSNum(ts), TSDenom(ts), TSClocks(ts),TS32nd(ts));
+}
 
- void TTimeConverter::Update (ULONG date_ticks, short num, short denom, short clock , short N32nd)
- {
- 	if ((num != fNumerator)   				// Update values only if the TimeSign has changed
-		|| (denom != fDenominator) 
+
+/*--------------------------------------------------------------------------*/
+
+  void TTimeConverter::Update (ULONG date_ticks, short num, short denom, short clock , short N32nd)
+  {
+  	if ((num != fNumerator)   	// Update values only if the TimeSign has changed
+		|| (denom != fDenominator)
 		|| (clock != fNClocks)
 		|| (N32nd != fN32nd)) {
- 
+
  		fLast_pos = ConvertTickToBBU (date_ticks);
-	
+
+  		// Check arguments validity
+ 		if ((num == 0) || (CalcBeat(fInt.fTicks_per_quarter, denom - 2) == 0)) {
+  			// Set default values
+  			num = kDefaultNum;
+			denom = kDefaultDenom;
+			clock = kDefaultClocks;
+			N32nd = kDefaultN32;
+		}
+
 		fInt.fBeat_per_bar =  num;
-		fInt.fTick_per_beat = CalcBeat (fInt.fTicks_per_quarter, denom - 2);
+		fInt.fTick_per_beat = CalcBeat(fInt.fTicks_per_quarter, denom - 2);
 		fInt.fTick_per_bar = fInt.fBeat_per_bar * fInt.fTick_per_beat;
-	
+
 		fNumerator = num;
 		fDenominator = denom;
 		fNClocks = clock;
 		fN32nd = N32nd;
-	
+
 		fLast_timesign = date_ticks;
 	}
- }
- 
+  }
+
+
 /*--------------------------------------------------------------------------*/
  
  TPos TTimeConverter::ConvertTickToBBU (ULONG date_ticks) 	
