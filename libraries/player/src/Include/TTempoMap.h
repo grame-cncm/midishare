34e4cc7d4c554a78f01b19f94e8ced2203985c60
letz <letz>
2003-01-27 13:23:32 +0000

    Replace pointer to converters with structures

--- libraries/player/src/Include/TTempoMap.h
+++ libraries/player/src/Include/TTempoMap.h
@@ -1,5 +1,3 @@
-
-
 // ===========================================================================
 // The Player Library is Copyright (c) Grame, Computer Music Research Laboratory 
 // 1996-1999, and is distributed as Open Source software under the Artistic License;
@@ -19,9 +17,6 @@
 /*! 
  \brief A class used to maintain the current Tempo and TimeSign state and provide conversion functions 
 */
-// TTempoMap: an object used to maintain the current Tempo and TimeSign state
-// and provide convertion functions 
-//
 
 #ifndef __TTempoMap__
 #define __TTempoMap__
@@ -31,25 +26,31 @@
 #include "TTimeConverter.h"
 
 
-//-----------------------
+//-----------------
 // Class TTempoMap
-//-----------------------
+//-----------------
 
 class TTempoMap  {
 
 	protected:
-	
-		ULONG	fCurdate_ticks;			// Current date in Ticks
-		TPos	fCur_pos;          		// Current position in Bar,Beat,Unit
-		ULONG	fCur_date_ten_micro;   	// Current date in 10 micro sec
 		
-		TTempoConverterPtr fTempoConverter;  // Tempo converter
-		TTimeConverterPtr  fTimeConverter;   // TimeSign converter
+		/*! Current date in Ticks */
+		ULONG	fCurdate_ticks;		
+		/*! Current position in Bar,Beat,Unit	*/
+		TPos	fCur_pos;         
+		/*! Current date in 10 micro sec 	*/	
+		ULONG	fCur_date_ten_micro;   	
+		
+		/*! Tempo converter */
+		TTempoConverter fTempoConverter;  
+		/*! TimeSign converter */
+		TTimeConverter  fTimeConverter;   
+			
 				
 	public :
  
  		TTempoMap(ULONG tpq);
- 		virtual ~TTempoMap ();
+ 		virtual ~TTempoMap (){}
  		
  		void Init ();
  		
@@ -58,37 +59,37 @@ class TTempoMap  {
 		ULONG CurDateMs(){ return fCur_date_ten_micro/100;}
 		const TPos CurDateBBU(){ return fCur_pos;}
 		
-		short GetNum ()     { return fTimeConverter->GetNum();}
-		short GetDenom ()   { return fTimeConverter->GetDenom();}
-		short GetNClocks()  { return fTimeConverter->GetNClocks();}
-		short GetN32nd()    { return fTimeConverter->GetN32nd();}
+		short GetNum ()     { return fTimeConverter.GetNum();}
+		short GetDenom ()   { return fTimeConverter.GetDenom();}
+		short GetNClocks()  { return fTimeConverter.GetNClocks();}
+		short GetN32nd()    { return fTimeConverter.GetN32nd();}
 			
-		ULONG GetTempo ()   { return fTempoConverter->GetTempo();}
-		void SetTempo (ULONG date_ticks,ULONG tempo) {fTempoConverter->Update(date_ticks,tempo); }
+		ULONG GetTempo ()   { return fTempoConverter.GetTempo();}
+		void SetTempo (ULONG date_ticks,ULONG tempo) {fTempoConverter.Update(date_ticks,tempo); }
 			
-		const TPos ConvertTickToBBU(ULONG date_tick) { return fTimeConverter->ConvertTickToBBU (date_tick);}
-		ULONG ConvertBBUToTick(const TPos& pos) { return fTimeConverter->ConvertBBUToTick (pos);}
+		const TPos ConvertTickToBBU(ULONG date_tick) { return fTimeConverter.ConvertTickToBBU (date_tick);}
+		ULONG ConvertBBUToTick(const TPos& pos) { return fTimeConverter.ConvertBBUToTick (pos);}
 		
-		ULONG ConvertTickToMs  (ULONG date_tick) { return fTempoConverter->ConvertTickToMs(date_tick);}
-		ULONG ConvertMsToTick  (ULONG date_ms)   { return fTempoConverter->ConvertMsToTick(date_ms);}
+		ULONG ConvertTickToMs  (ULONG date_tick) { return fTempoConverter.ConvertTickToMs(date_tick);}
+		ULONG ConvertMsToTick  (ULONG date_ms)   { return fTempoConverter.ConvertMsToTick(date_ms);}
 		
-		ULONG ConvertTickToMicroSec  (ULONG date_tick) { return fTempoConverter->ConvertTickToMicroSec(date_tick);}
-		ULONG ConvertMicroSecToTick  (ULONG date_ms)   { return fTempoConverter->ConvertMicroSecToTick(date_ms);}
+		ULONG ConvertTickToMicroSec  (ULONG date_tick) { return fTempoConverter.ConvertTickToMicroSec(date_tick);}
+		ULONG ConvertMicroSecToTick  (ULONG date_ms)   { return fTempoConverter.ConvertMicroSecToTick(date_ms);}
 		
 		void UpdateBBU (const TPos& pos);
 		void UpdateTicks (ULONG date_ticks);
 		void UpdateTenMicroSec (ULONG date_ten_micro);	
 		void UpdateMs (ULONG date_ms);	
 			
-		Boolean SupEq(ULONG date_ticks, ULONG date_ten_micro) 	{ return  fTempoConverter->ConvertTickToMicroSec(date_ticks) >= date_ten_micro;}
-		Boolean Sup(ULONG date_ticks, ULONG date_ten_micro) 	{ return  fTempoConverter->ConvertTickToMicroSec(date_ticks) > date_ten_micro;}
-		Boolean InfEq(ULONG date_ticks, ULONG date_ten_micro) 	{ return  fTempoConverter->ConvertTickToMicroSec(date_ticks) <= date_ten_micro;}
-		Boolean Inf(ULONG date_ticks, ULONG date_ten_micro) 	{ return  fTempoConverter->ConvertTickToMicroSec(date_ticks) < date_ten_micro;}
+		Boolean SupEq(ULONG date_ticks, ULONG date_ten_micro) 	{ return  fTempoConverter.ConvertTickToMicroSec(date_ticks) >= date_ten_micro;}
+		Boolean Sup(ULONG date_ticks, ULONG date_ten_micro) 	{ return  fTempoConverter.ConvertTickToMicroSec(date_ticks) > date_ten_micro;}
+		Boolean InfEq(ULONG date_ticks, ULONG date_ten_micro) 	{ return  fTempoConverter.ConvertTickToMicroSec(date_ticks) <= date_ten_micro;}
+		Boolean Inf(ULONG date_ticks, ULONG date_ten_micro) 	{ return  fTempoConverter.ConvertTickToMicroSec(date_ticks) < date_ten_micro;}
 		
-		Boolean SupEq(ULONG date_ticks, const TPos& pos) 	{ return  fTimeConverter->SupEq(fTimeConverter->ConvertTickToBBU(date_ticks), pos);}
-		Boolean Sup(ULONG date_ticks, const TPos& pos) 		{ return  fTimeConverter->Sup(fTimeConverter->ConvertTickToBBU(date_ticks), pos);}
-		Boolean InfEq(ULONG date_ticks, const TPos& pos) 	{ return  fTimeConverter->InfEq(fTimeConverter->ConvertTickToBBU(date_ticks), pos);}
-		Boolean Inf(ULONG date_ticks, const TPos& pos) 		{ return  fTimeConverter->Inf(fTimeConverter->ConvertTickToBBU(date_ticks), pos);}
+		Boolean SupEq(ULONG date_ticks, const TPos& pos) 	{ return  fTimeConverter.SupEq(fTimeConverter.ConvertTickToBBU(date_ticks), pos);}
+		Boolean Sup(ULONG date_ticks, const TPos& pos) 		{ return  fTimeConverter.Sup(fTimeConverter.ConvertTickToBBU(date_ticks), pos);}
+		Boolean InfEq(ULONG date_ticks, const TPos& pos) 	{ return  fTimeConverter.InfEq(fTimeConverter.ConvertTickToBBU(date_ticks), pos);}
+		Boolean Inf(ULONG date_ticks, const TPos& pos) 		{ return  fTimeConverter.Inf(fTimeConverter.ConvertTickToBBU(date_ticks), pos);}
 		
 };
 
