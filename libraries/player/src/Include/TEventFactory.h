4dcbb6d9e0d6cc96a1a85d43971b570a801fdef0
letz <letz>
2003-01-27 13:39:04 +0000

    Static functions for event creation

--- libraries/player/src/Include/TEventFactory.h
+++ libraries/player/src/Include/TEventFactory.h
@@ -1,6 +1,6 @@
 // ===========================================================================
 // The Player Library is Copyright (c) Grame, Computer Music Research Laboratory 
-// 1996-1999, and is distributed as Open Source software under the Artistic License;
+// 1996-2001, and is distributed as Open Source software under the Artistic License;
 // see the file "Artistic" that is included in the distribution for details.
 //
 // Grame : Computer Music Research Laboratory
@@ -11,9 +11,9 @@
 // ===========================================================================
 //	TEventFactory.h			    
 // ===========================================================================
-//
-// A TEventFactory object allows to build TEvent objects using the MidiShare event type.
-//
+/*!
+\brief A TEventFactory object allows to build TEvent objects using the MidiShare event type.
+*/
 
 
 
@@ -31,9 +31,24 @@ typedef TEventFactory FAR * TEventFactoryPtr;
 
 /*--------------------------------------------------------------------------*/
 
-typedef  TEventPtr (TEventFactory::*mthptr)(MidiEvPtr e);
+typedef  TEventPtr  (*mthptr)(MidiEvPtr e);
 
-
+/*--------------------------------------------------------------------------*/
+/* Event creation methods */
+static TEventPtr CreateEvent(MidiEvPtr e)		{ return new TEvent(e);}
+static TEventPtr CreateNote(MidiEvPtr e)		{ return new TNote(e);}
+static TEventPtr CreateKeyOn(MidiEvPtr e)		{ return new TKeyOn(e);}
+static TEventPtr CreateKeyOff(MidiEvPtr e)		{ return new TKeyOff(e);}
+static TEventPtr CreateKeyPress(MidiEvPtr e)	{ return new TKeyPress(e);}
+static TEventPtr CreateCtrlChange(MidiEvPtr e)	{ return new TCtrlChange(e);}
+static TEventPtr CreateProgChange(MidiEvPtr e)	{ return new TProgChange(e);}
+static TEventPtr CreateChanPress(MidiEvPtr e)	{ return new TChanPress(e);}
+static TEventPtr CreatePitchBend(MidiEvPtr e)	{ return new TPitchBend(e);}
+static TEventPtr CreateTune(MidiEvPtr e)		{ return new TTune(e);}
+static TEventPtr CreateTempo(MidiEvPtr e)		{ return new TTempo(e);}
+static TEventPtr CreateTimeSign(MidiEvPtr e)	{ return new TTimeSign(e);}
+static TEventPtr CreateSysEx(MidiEvPtr e)		{ return new TSysEx(e);}
+		
 //-----------------------
 // Class TEventFactory 
 //-----------------------
@@ -41,23 +56,8 @@ typedef  TEventPtr (TEventFactory::*mthptr)(MidiEvPtr e);
 class TEventFactory {
 
 	private:
-	
-		TEventPtr 	CreateEvent(MidiEvPtr e){ return new TEvent(e);}
 		
-		TEventPtr 	CreateNote(MidiEvPtr e){ return new TNote(e);}
-		TEventPtr 	CreateKeyOn(MidiEvPtr e){ return new TKeyOn(e);}
-		TEventPtr 	CreateKeyOff(MidiEvPtr e){ return new TKeyOff(e);}
-		TEventPtr	CreateKeyPress(MidiEvPtr e){ return new TKeyPress(e);}
-		TEventPtr 	CreateCtrlChange(MidiEvPtr e){ return new TCtrlChange(e);}
-		TEventPtr   CreateProgChange(MidiEvPtr e){ return new TProgChange(e);}
-		TEventPtr   CreateChanPress(MidiEvPtr e){ return new TChanPress(e);}
-		TEventPtr   CreatePitchBend(MidiEvPtr e){ return new TPitchBend(e);}
-		TEventPtr 	CreateTune(MidiEvPtr e){ return new TTune(e);}
-		TEventPtr 	CreateTempo(MidiEvPtr e){ return new TTempo(e);}
-		TEventPtr 	CreateTimeSign(MidiEvPtr e){ return new TTimeSign(e);}
-		TEventPtr 	CreateSysEx(MidiEvPtr e){ return new TSysEx(e);}
-		
-		mthptr 	fBuildTable[256];
+		mthptr fBuildTable[256];
 		static TEventFactoryPtr fInstance;
 	
 	public: 
@@ -65,9 +65,7 @@ class TEventFactory {
 		TEventFactory ();
 		~TEventFactory() {}
 		
-		static TEventPtr	GenericCreateEvent(MidiEvPtr e) {return fInstance->GenericCreateEventAux(e);}
-		       TEventPtr	GenericCreateEventAux(MidiEvPtr e) {return (this->*fBuildTable[EvType(e)])(e);}
-		
+		static TEventPtr GenericCreateEvent(MidiEvPtr e) {return fInstance->fBuildTable[EvType(e)](e);}
 		static void Init() { if (fInstance == 0) fInstance = new TEventFactory(); }
 };
 
