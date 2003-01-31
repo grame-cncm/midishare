e3f22062b43a0795bfb41217ad631cddba69dc57
fober <fober>
2003-01-31 19:53:56 +0000

    windows export declaration

--- src/common/Headers/msFunctions.h
+++ src/common/Headers/msFunctions.h
@@ -27,6 +27,12 @@
 
 #include "msDefs.h"
 
+#if defined(WIN32) && defined(MSExport)
+#  define MSAPI(type)	_declspec (dllexport) type
+#else
+#  define MSAPI(type)	type
+#endif
+
 /*******************************************************************************
 *                                                               ENTRY POINTS
 *******************************************************************************/
@@ -39,128 +45,128 @@ extern "C" {
 #endif
 
 /*--------------------------- Global MidiShare environment --------------------*/
-  short          MidiGetVersion   (void);
+  MSAPI(short)          MidiGetVersion   (void);
 
-  short          MidiCountAppls   (void);
-  short          MidiGetIndAppl   (short index);
-  short          MidiGetNamedAppl (MidiName name);
+  MSAPI(short)          MidiCountAppls   (void);
+  MSAPI(short)          MidiGetIndAppl   (short index);
+  MSAPI(short)          MidiGetNamedAppl (MidiName name);
 
 /*----------------------------- SMPTE synchronization -------------------------*/
-  void           MidiGetSyncInfo (SyncInfoPtr p);
-  void           MidiSetSyncMode (unsigned short mode);
-  long           MidiGetExtTime  (void);
-  long           MidiInt2ExtTime (long xl);
-  long           MidiExt2IntTime (long xl);
-  void           MidiTime2Smpte  (long time, short format, SmpteLocPtr loc);
-  long           MidiSmpte2Time  (SmpteLocPtr loc);
+  MSAPI(void)           MidiGetSyncInfo (SyncInfoPtr p);
+  MSAPI(void)           MidiSetSyncMode (unsigned short mode);
+  MSAPI(long)           MidiGetExtTime  (void);
+  MSAPI(long)           MidiInt2ExtTime (long xl);
+  MSAPI(long)           MidiExt2IntTime (long xl);
+  MSAPI(void)           MidiTime2Smpte  (long time, short format, SmpteLocPtr loc);
+  MSAPI(long)           MidiSmpte2Time  (SmpteLocPtr loc);
 
 /*----------------------------- Open / close application ----------------------*/
-  short          MidiOpen  (MidiName applName);
-  void           MidiClose (short refNum);
+  MSAPI(short)          MidiOpen  (MidiName applName);
+  MSAPI(void)           MidiClose (short refNum);
 
 /*--------------------------- Application configuration -----------------------*/
-  MidiName       MidiGetName(short refNum);
-  void           MidiSetName(short refNum, MidiName applName);
-  void*          MidiGetInfo(short refNum);
-  void           MidiSetInfo(short refNum,void* InfoZone);
-  RcvAlarmPtr    MidiGetRcvAlarm(short refNum);
-  void           MidiSetRcvAlarm(short refNum, RcvAlarmPtr alarm);
-  ApplAlarmPtr   MidiGetApplAlarm(short refNum);
-  void           MidiSetApplAlarm(short refNum, ApplAlarmPtr alarm);
+  MSAPI(MidiName)       MidiGetName(short refNum);
+  MSAPI(void)           MidiSetName(short refNum, MidiName applName);
+  MSAPI(void*)          MidiGetInfo(short refNum);
+  MSAPI(void)           MidiSetInfo(short refNum,void* InfoZone);
+  MSAPI(RcvAlarmPtr)    MidiGetRcvAlarm(short refNum);
+  MSAPI(void)           MidiSetRcvAlarm(short refNum, RcvAlarmPtr alarm);
+  MSAPI(ApplAlarmPtr)   MidiGetApplAlarm(short refNum);
+  MSAPI(void)           MidiSetApplAlarm(short refNum, ApplAlarmPtr alarm);
 
 /*----------------------------- Filters configuration -------------------------*/
-  MidiFilterPtr  MidiGetFilter(short refNum);
-  void           MidiSetFilter(short refNum, MidiFilterPtr f);
+  MSAPI(MidiFilterPtr)  MidiGetFilter(short refNum);
+  MSAPI(void)           MidiSetFilter(short refNum, MidiFilterPtr f);
  
-  MidiFilterPtr  MidiNewFilter (void);
-  void           MidiFreeFilter (MidiFilterPtr);
-  void           MidiAcceptPort(MidiFilterPtr f, short port, Boolean);
-  void           MidiAcceptChan(MidiFilterPtr f, short chan, Boolean);
-  void           MidiAcceptType(MidiFilterPtr f, short type, Boolean);
-  Boolean        MidiIsAcceptedPort(MidiFilterPtr f, short port);
-  Boolean        MidiIsAcceptedChan(MidiFilterPtr f, short chan);
-  Boolean        MidiIsAcceptedType(MidiFilterPtr f, short type);
+  MSAPI(MidiFilterPtr)  MidiNewFilter (void);
+  MSAPI(void)           MidiFreeFilter (MidiFilterPtr);
+  MSAPI(void)           MidiAcceptPort(MidiFilterPtr f, short port, Boolean);
+  MSAPI(void)           MidiAcceptChan(MidiFilterPtr f, short chan, Boolean);
+  MSAPI(void)           MidiAcceptType(MidiFilterPtr f, short type, Boolean);
+  MSAPI(Boolean)        MidiIsAcceptedPort(MidiFilterPtr f, short port);
+  MSAPI(Boolean)        MidiIsAcceptedChan(MidiFilterPtr f, short chan);
+  MSAPI(Boolean)        MidiIsAcceptedType(MidiFilterPtr f, short type);
 
 /*------------------------- Inter-Application Connections ---------------------*/
-  void           MidiConnect(short src, short dest , Boolean state);
-  Boolean        MidiIsConnected(short src, short dest);
+  MSAPI(void)           MidiConnect(short src, short dest , Boolean state);
+  MSAPI(Boolean)        MidiIsConnected(short src, short dest);
 
 /*-------------------------- Events and memory managing -----------------------*/
-  unsigned long  MidiFreeSpace(void);
-  unsigned long  MidiDesiredSpace(void);
-  unsigned long  MidiTotalSpace(void);
-  unsigned long  MidiGrowSpace(long n);
+  MSAPI(unsigned long)  MidiFreeSpace(void);
+  MSAPI(unsigned long)  MidiDesiredSpace(void);
+  MSAPI(unsigned long)  MidiTotalSpace(void);
+  MSAPI(unsigned long)  MidiGrowSpace(long n);
 
-  MidiEvPtr      MidiNewCell(void);
-  void           MidiFreeCell(MidiEvPtr e);
+  MSAPI(MidiEvPtr)      MidiNewCell(void);
+  MSAPI(void)           MidiFreeCell(MidiEvPtr e);
 
-  MidiEvPtr      MidiNewEv(short typeNum);
-  MidiEvPtr      MidiCopyEv(MidiEvPtr e);
-  void           MidiFreeEv(MidiEvPtr e);
+  MSAPI(MidiEvPtr)      MidiNewEv(short typeNum);
+  MSAPI(MidiEvPtr)      MidiCopyEv(MidiEvPtr e);
+  MSAPI(void)           MidiFreeEv(MidiEvPtr e);
 
-  void           MidiSetField(MidiEvPtr e, long f, long v);
-  long           MidiGetField(MidiEvPtr e, long f);
-  void           MidiAddField(MidiEvPtr e, long v);
-  long           MidiCountFields(MidiEvPtr e);
+  MSAPI(void)           MidiSetField(MidiEvPtr e, long f, long v);
+  MSAPI(long)           MidiGetField(MidiEvPtr e, long f);
+  MSAPI(void)           MidiAddField(MidiEvPtr e, long v);
+  MSAPI(long)           MidiCountFields(MidiEvPtr e);
 
-  MidiEvPtr      MidiGetLink        (MidiEvPtr e);
-  void           MidiSetLink        (MidiEvPtr e, MidiEvPtr next);
+  MSAPI(MidiEvPtr)      MidiGetLink        (MidiEvPtr e);
+  MSAPI(void)           MidiSetLink        (MidiEvPtr e, MidiEvPtr next);
 
-  long           MidiGetDate        (MidiEvPtr e);
-  void           MidiSetDate        (MidiEvPtr e, long);
-  short          MidiGetRefNum      (MidiEvPtr e);
-  void           MidiSetRefNum      (MidiEvPtr e, short);
-  short          MidiGetType        (MidiEvPtr e);
-  void           MidiSetType        (MidiEvPtr e, short);
-  short          MidiGetChan        (MidiEvPtr e);
-  void           MidiSetChan        (MidiEvPtr e, short);
-  short          MidiGetPort        (MidiEvPtr e);
-  void           MidiSetPort        (MidiEvPtr e, short);
+  MSAPI(long)           MidiGetDate        (MidiEvPtr e);
+  MSAPI(void)           MidiSetDate        (MidiEvPtr e, long);
+  MSAPI(short)          MidiGetRefNum      (MidiEvPtr e);
+  MSAPI(void)           MidiSetRefNum      (MidiEvPtr e, short);
+  MSAPI(short)          MidiGetType        (MidiEvPtr e);
+  MSAPI(void)           MidiSetType        (MidiEvPtr e, short);
+  MSAPI(short)          MidiGetChan        (MidiEvPtr e);
+  MSAPI(void)           MidiSetChan        (MidiEvPtr e, short);
+  MSAPI(short)          MidiGetPort        (MidiEvPtr e);
+  MSAPI(void)           MidiSetPort        (MidiEvPtr e, short);
 
-  long           MidiGetData0       (MidiEvPtr e);
-  long           MidiGetData1       (MidiEvPtr e);
-  long           MidiGetData2       (MidiEvPtr e);
-  long           MidiGetData3       (MidiEvPtr e);
-  void           MidiSetData0       (MidiEvPtr e, long);
-  void           MidiSetData1       (MidiEvPtr e, long);
-  void           MidiSetData2       (MidiEvPtr e, long);
-  void           MidiSetData3       (MidiEvPtr e, long);
+  MSAPI(long)           MidiGetData0       (MidiEvPtr e);
+  MSAPI(long)           MidiGetData1       (MidiEvPtr e);
+  MSAPI(long)           MidiGetData2       (MidiEvPtr e);
+  MSAPI(long)           MidiGetData3       (MidiEvPtr e);
+  MSAPI(void)           MidiSetData0       (MidiEvPtr e, long);
+  MSAPI(void)           MidiSetData1       (MidiEvPtr e, long);
+  MSAPI(void)           MidiSetData2       (MidiEvPtr e, long);
+  MSAPI(void)           MidiSetData3       (MidiEvPtr e, long);
 
 
 /*------------------------------- Sequence managing ---------------------------*/
-  MidiSeqPtr     MidiNewSeq   (void);
-  void           MidiAddSeq   (MidiSeqPtr s, MidiEvPtr e);
-  void           MidiFreeSeq  (MidiSeqPtr s);
-  void           MidiClearSeq (MidiSeqPtr s);
-  void           MidiApplySeq (MidiSeqPtr s, ApplyProcPtr proc);
+  MSAPI(MidiSeqPtr)     MidiNewSeq   (void);
+  MSAPI(void)           MidiAddSeq   (MidiSeqPtr s, MidiEvPtr e);
+  MSAPI(void)           MidiFreeSeq  (MidiSeqPtr s);
+  MSAPI(void)           MidiClearSeq (MidiSeqPtr s);
+  MSAPI(void)           MidiApplySeq (MidiSeqPtr s, ApplyProcPtr proc);
 
-  MidiEvPtr      MidiGetFirstEv (MidiSeqPtr s);
-  MidiEvPtr      MidiGetLastEv  (MidiSeqPtr s);
-  void           MidiSetFirstEv (MidiSeqPtr s, MidiEvPtr e);
-  void           MidiSetLastEv  (MidiSeqPtr s, MidiEvPtr e);
+  MSAPI(MidiEvPtr)      MidiGetFirstEv (MidiSeqPtr s);
+  MSAPI(MidiEvPtr)      MidiGetLastEv  (MidiSeqPtr s);
+  MSAPI(void)           MidiSetFirstEv (MidiSeqPtr s, MidiEvPtr e);
+  MSAPI(void)           MidiSetLastEv  (MidiSeqPtr s, MidiEvPtr e);
 
 /*------------------------------------- Time ----------------------------------*/
-  unsigned long  MidiGetTime (void);
+  MSAPI(unsigned long)  MidiGetTime (void);
 
 /*------------------------------------ Sending --------------------------------*/
-  void           MidiSendIm (short refNum, MidiEvPtr e);
-  void           MidiSend   (short refNum, MidiEvPtr e);
-  void           MidiSendAt (short refNum, MidiEvPtr e, unsigned long d);
+  MSAPI(void)           MidiSendIm (short refNum, MidiEvPtr e);
+  MSAPI(void)           MidiSend   (short refNum, MidiEvPtr e);
+  MSAPI(void)           MidiSendAt (short refNum, MidiEvPtr e, unsigned long d);
 
 /*------------------------------------ Receving -------------------------------*/
-  unsigned long  MidiCountEvs (short refNum);
-  MidiEvPtr      MidiGetEv    (short refNum);
-  MidiEvPtr      MidiAvailEv  (short refNum);
-  void           MidiFlushEvs (short refNum);
+  MSAPI(unsigned long)  MidiCountEvs (short refNum);
+  MSAPI(MidiEvPtr)      MidiGetEv    (short refNum);
+  MSAPI(MidiEvPtr)      MidiAvailEv  (short refNum);
+  MSAPI(void)           MidiFlushEvs (short refNum);
 
 /*------------------------------- Drivers management --------------------------*/
 #ifdef MSKernel
   short          MidiRegisterDriver   (TDriverInfos * infos, TDriverOperation *op);
   void           MidiUnregisterDriver (short refnum);
 #endif
-  short          MidiCountDrivers     (void);
-  short          MidiGetIndDriver     (short index);
-  Boolean        MidiGetDriverInfos   (short refnum, TDriverInfos * infos);
+  MSAPI(short)          MidiCountDrivers     (void);
+  MSAPI(short)          MidiGetIndDriver     (short index);
+  MSAPI(Boolean)        MidiGetDriverInfos   (short refnum, TDriverInfos * infos);
 
 /*-------------------------------- Slots management ---------------------------*/
 #ifdef MSKernel
@@ -168,26 +174,26 @@ extern "C" {
   void           MidiSetSlotName (SlotRefNum slot, MidiName name);
   void           MidiRemoveSlot  (SlotRefNum slot);
 #endif
-  SlotRefNum     MidiGetIndSlot  (short refnum, short index);
-  Boolean        MidiGetSlotInfos    (SlotRefNum slot, TSlotInfos * infos);
-  void           MidiConnectSlot     (short port, SlotRefNum slot, Boolean state);
-  Boolean        MidiIsSlotConnected (short port, SlotRefNum slot);
+  MSAPI(SlotRefNum)     MidiGetIndSlot  (short refnum, short index);
+  MSAPI(Boolean)        MidiGetSlotInfos    (SlotRefNum slot, TSlotInfos * infos);
+  MSAPI(void)           MidiConnectSlot     (short port, SlotRefNum slot, Boolean state);
+  MSAPI(Boolean)        MidiIsSlotConnected (short port, SlotRefNum slot);
 
 /*----------------------------------- Mail boxes ------------------------------*/
-  void*          MidiReadSync  (void ** adrMem);
-  void*          MidiWriteSync (void ** adrMem, void* val);
+  MSAPI(void*)          MidiReadSync  (void ** adrMem);
+  MSAPI(void*)          MidiWriteSync (void ** adrMem, void* val);
 
 /*---------------------------------- Task Managing ----------------------------*/
-  void           MidiCall (TaskPtr routine, unsigned long date, short refNum, long a1,long a2,long a3);
-  MidiEvPtr      MidiTask (TaskPtr routine, unsigned long date, short refNum, long a1,long a2,long a3);
-  MidiEvPtr      MidiDTask(TaskPtr routine, unsigned long date, short refNum, long a1,long a2,long a3);
-  void           MidiForgetTask(MidiEvPtr FAR *e);
-  long           MidiCountDTasks(short refNum);
-  void           MidiFlushDTasks(short refNum);
-  void           MidiExec1DTask(short refNum);
+  MSAPI(void)           MidiCall (TaskPtr routine, unsigned long date, short refNum, long a1,long a2,long a3);
+  MSAPI(MidiEvPtr)      MidiTask (TaskPtr routine, unsigned long date, short refNum, long a1,long a2,long a3);
+  MSAPI(MidiEvPtr)      MidiDTask(TaskPtr routine, unsigned long date, short refNum, long a1,long a2,long a3);
+  MSAPI(void)           MidiForgetTask(MidiEvPtr FAR *e);
+  MSAPI(long)           MidiCountDTasks(short refNum);
+  MSAPI(void)           MidiFlushDTasks(short refNum);
+  MSAPI(void)           MidiExec1DTask(short refNum);
 
 /*----------------------------------- MidiShare -------------------------------*/
-  Boolean        MidiShare(void);
+  MSAPI(Boolean)        MidiShare(void);
 
 #ifdef __cplusplus
 }
