7a60b63cca47ef62e1da35718df3657e27cc4321
letz <letz>
2003-02-22 20:05:20 +0000

    Apply Michael Stauffer corrections

--- src/mac/Include/MidiSharePPC-Carbon.h
+++ src/mac/Include/MidiSharePPC-Carbon.h
@@ -1,13 +1,14 @@
+
 /*
   Copyright © Grame 1999-2002
 
-  This program is free software; you can redistribute it and modify it under 
-  the terms of the GNU General Public License as published by the 
+  This program is free software; you can redistribute it and modify it under
+  the terms of the GNU General Public License as published by the
   Free Software Foundation version 2 of the License.
 
   This program is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
   more details.
 
   You should have received a copy of the GNU General Public License
@@ -16,106 +17,64 @@
 
   Grame Research Laboratory, 9, rue du Garet 69001 Lyon - France
   grame@grame.fr
-
+  
+  S Letz, M Stauffer, Stefan Csomor
+  
 */
 
 #ifndef __MidiSharePPC_Carbon__
 #define __MidiSharePPC_Carbon__
 
-#include <Carbon.h>
+#include <MixedMode.h> 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	
+
 /*
-Re-implement a special CallUniversalProc, NewRoutineDescriptor and DisposeRoutineDescriptor
+Re-implement a special CallUniversalProc, NewRoutineDescriptor and
+DisposeRoutineDescriptor
 */
 
-typedef long (*CallUniversalProcProcPtr)( UniversalProcPtr theProcPtr, ProcInfoType procInfo,...) ;
+typedef long (*CallUniversalProcProcPtr)( UniversalProcPtr theProcPtr,
+ProcInfoType procInfo,...) ;
 #define CallUniversalProc (*tCallUniversalProcProcPtr)
 
 
-typedef UniversalProcPtr (*NewRoutineDescriptorProcPtr)(ProcPtr theProc, ProcInfoType , ISAType );
+typedef UniversalProcPtr (*NewRoutineDescriptorProcPtr)(ProcPtr theProc,
+ProcInfoType , ISAType );
 #define NewRoutineDescriptor (*tNewRoutineDescriptorProcPtr)
 
 typedef void (*DisposeRoutineDescriptorProcPtr)(UniversalProcPtr);
 #define DisposeRoutineDescriptor (*tDisposeRoutineDescriptorProcPtr)
 
 /*
-Use global static variables which contains the real CallUniversalProc, NewRoutineDescriptor and DisposeRoutineDescriptor entry points
+Use global static variables which contains the real CallUniversalProc,
+NewRoutineDescriptor and DisposeRoutineDescriptor entry points
 */
 
-static CallUniversalProcProcPtr tCallUniversalProcProcPtr = nil;
-static NewRoutineDescriptorProcPtr tNewRoutineDescriptorProcPtr = nil;
-static DisposeRoutineDescriptorProcPtr tDisposeRoutineDescriptorProcPtr = nil;
+extern CallUniversalProcProcPtr tCallUniversalProcProcPtr ;
+extern NewRoutineDescriptorProcPtr tNewRoutineDescriptorProcPtr ;
+extern DisposeRoutineDescriptorProcPtr tDisposeRoutineDescriptorProcPtr;
 
 
-/* 
+/*
 Include the standard MidiShare.h file
 */
-	
-#include "MidiSharePPC-68k.h" 
 
-
-/*
-Load the real CallUniversalProc, NewRoutineDescriptor and DisposeRoutineDescriptor entry points
-*/
-
-void Load_FunctionProc () 
-{
-	CFragConnectionID  			connectionID;
-	Ptr                			mainAddress;
-	Str255             			errorString;
-	OSErr              			anErr;
-
-	if (nil == tCallUniversalProcProcPtr)	/* if it's not defined yet */
-	{
-		/* Resolve the symbol against InterfaceLib. */
-		anErr = GetSharedLibrary("\pInterfaceLib", kPowerPCCFragArch, kFindCFrag, &connectionID, &mainAddress, errorString);
-		if (noErr == anErr)
-		{
-				CFragSymbolClass    symbolClass;
-		        anErr = FindSymbol(connectionID, "\pCallUniversalProc",
-		       			(Ptr *) &tCallUniversalProcProcPtr,
-									&symbolClass);
-		        if (noErr != anErr)
-		       	tCallUniversalProcProcPtr = nil;
-		       	
-		        anErr = FindSymbol(connectionID, "\pNewRoutineDescriptor",
-		       			(Ptr *) &tNewRoutineDescriptorProcPtr,
-									&symbolClass);
-		        if (noErr != anErr)
-		       	tNewRoutineDescriptorProcPtr = nil;
-		       	
-		       	anErr = FindSymbol(connectionID, "\pDisposeRoutineDescriptor",
-		       			(Ptr *) &tDisposeRoutineDescriptorProcPtr,
-									&symbolClass);
-		        if (noErr != anErr)
-		       	tDisposeRoutineDescriptorProcPtr = nil;
-		}
-		
-		
-	}
-	
-}
+#include "MidiSharePPC-68k.h"
 
 /*
-Re-define the MidiShare() function to call the Load_FunctionProc, since MidiShare() is supposed to be called
-before any other MidiShare function.
-
+Re-define the MidiShare() function to call the Load_FunctionProc, since
+MidiShare() is supposed to be called before any other MidiShare function.
 Otherwise Load_FunctionProc can be called at initialisation time.
 */
 
-
 #ifdef MidiShare
 #undef MidiShare
-int MidiShare()
-{
-	 Load_FunctionProc();
-	 return	( **((long**)0xB8) == 0xD080D080 );
-}
+extern int MidiShare() ;
+extern void Load_FunctionProc (); 
 #endif
 
 #ifdef __cplusplus
