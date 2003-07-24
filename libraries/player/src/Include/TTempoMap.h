cfd67e5cdcc8f466373546671f6cd28eeeb6f474
letz <letz>
2003-07-24 15:36:35 +0000

    Cleanup

--- libraries/player/src/Include/TTempoMap.h
+++ libraries/player/src/Include/TTempoMap.h
@@ -52,32 +52,32 @@ class TTempoMap {
  		
  		void Init ();
  		
- 		ULONG CurDateTicks(){ return fCurdate_ticks ;}
-		ULONG CurDateMicro(){ return fCur_date_ten_micro;}
-		ULONG CurDateMs(){ return fCur_date_ten_micro/100;}
-		const TPos CurDateBBU(){ return fCur_pos;}
+ 		ULONG CurDateTicks() {return fCurdate_ticks;}
+		ULONG CurDateMicro() {return fCur_date_ten_micro;}
+		ULONG CurDateMs() {return fCur_date_ten_micro/100;}
+		const TPos CurDateBBU() {return fCur_pos;}
 		
-		short GetNum ()     {return fTimeConverter.GetNum();}
-		short GetDenom ()   {return fTimeConverter.GetDenom();}
+		short GetNum()     {return fTimeConverter.GetNum();}
+		short GetDenom()   {return fTimeConverter.GetDenom();}
 		short GetNClocks()  {return fTimeConverter.GetNClocks();}
 		short GetN32nd()    {return fTimeConverter.GetN32nd();}
 			
-		ULONG GetTempo ()   {return fTempoConverter.GetTempo();}
-		void SetTempo (ULONG date_ticks,ULONG tempo) {fTempoConverter.Update(date_ticks,tempo);}
+		ULONG GetTempo()   {return fTempoConverter.GetTempo();}
+		void SetTempo(ULONG date_ticks,ULONG tempo) {fTempoConverter.Update(date_ticks,tempo);}
 			
 		const TPos ConvertTickToBBU(ULONG date_tick) {return fTimeConverter.ConvertTickToBBU (date_tick);}
 		ULONG ConvertBBUToTick(const TPos& pos) {return fTimeConverter.ConvertBBUToTick (pos);}
 		
-		ULONG ConvertTickToMs  (ULONG date_tick) {return fTempoConverter.ConvertTickToMs(date_tick);}
-		ULONG ConvertMsToTick  (ULONG date_ms)   {return fTempoConverter.ConvertMsToTick(date_ms);}
+		ULONG ConvertTickToMs(ULONG date_tick) {return fTempoConverter.ConvertTickToMs(date_tick);}
+		ULONG ConvertMsToTick(ULONG date_ms) {return fTempoConverter.ConvertMsToTick(date_ms);}
 		
-		ULONG ConvertTickToMicroSec  (ULONG date_tick) {return fTempoConverter.ConvertTickToMicroSec(date_tick);}
-		ULONG ConvertMicroSecToTick  (ULONG date_ms)   {return fTempoConverter.ConvertMicroSecToTick(date_ms);}
+		ULONG ConvertTickToMicroSec(ULONG date_tick) {return fTempoConverter.ConvertTickToMicroSec(date_tick);}
+		ULONG ConvertMicroSecToTick(ULONG date_ms)   {return fTempoConverter.ConvertMicroSecToTick(date_ms);}
 		
-		void UpdateBBU (const TPos& pos);
-		void UpdateTicks (ULONG date_ticks);
-		void UpdateTenMicroSec (ULONG date_ten_micro);	
-		void UpdateMs (ULONG date_ms);	
+		void UpdateBBU(const TPos& pos);
+		void UpdateTicks(ULONG date_ticks);
+		void UpdateTenMicroSec(ULONG date_ten_micro);	
+		void UpdateMs(ULONG date_ms);	
 			
 		Boolean SupEq(ULONG date_ticks, ULONG date_ten_micro) 	{return  fTempoConverter.ConvertTickToMicroSec(date_ticks) >= date_ten_micro;}
 		Boolean Sup(ULONG date_ticks, ULONG date_ten_micro) 	{return  fTempoConverter.ConvertTickToMicroSec(date_ticks) > date_ten_micro;}
