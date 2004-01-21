e0f79eb468641acc5b3f82573301e13828374d17
fober <fober>
2004-01-21 10:19:42 +0000

    version change

--- src/macosx-mono/Drivers/msQTDriver/msQTDriver.h
+++ src/macosx-mono/Drivers/msQTDriver/msQTDriver.h
@@ -24,7 +24,7 @@
 #ifndef __msQTDriver__
 #define __msQTDriver__
 
-#define kQTDriverVersion      101
+#define kQTDriverVersion      103
 #define QTDriverName	"QuickTime Driver"
 
 Boolean CheckQuickTime ();
