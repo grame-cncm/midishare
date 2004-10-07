99d256a250815ccc6bd5ec157f7eb0765f80bcfc
fober <fober>
2004-10-07 16:37:16 +0000

    no message

--- libraries/player/src/Library/Player.cpp
+++ libraries/player/src/Library/Player.cpp
@@ -1,20 +1,30 @@
-/* ===========================================================================
-// The Player Library is Copyright (c) Grame, Computer Music Research Laboratory 
-// 1996-1999, and is distributed as Open Source software under the Artistic License;
-// see the file "Artistic" that is included in the distribution for details.
-//
-// Grame : Computer Music Research Laboratory
-// Web : http://www.grame.fr/Research
-// E-mail : MidiShare@rd.grame.fr
-// ===========================================================================
+/*
 
+  Copyright © Grame 1996-2004
+
+  This library is free software; you can redistribute it and modify it under 
+  the terms of the GNU Library General Public License as published by the 
+  Free Software Foundation version 2 of the License, or any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License 
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Grame Research Laboratory, 9, rue du Garet 69001 Lyon - France
+  research@grame.fr
+
+*/
 
 // ===========================================================================
 //	Player.cpp			    
 // ===========================================================================
 //
 //	The C++/C interface to the Player API
-/*/
 
 #include  "TPlayer.h"
 
@@ -32,72 +42,73 @@
 
 // Player structure management
 
-short  EXPORT Version ();
+short  EXPORT Version();
 
 // Player structure management
 
-short  EXPORT OpenPlayer (MidiName name);
-void   EXPORT ClosePlayer (short refnum);
+short  EXPORT OpenPlayer(MidiName name);
+void   EXPORT ClosePlayer(short refnum);
 
 // Transport control
 
-void  EXPORT StartPlayer (short refnum);
-void  EXPORT ContPlayer (short refnum);
-void  EXPORT StopPlayer (short refnum);
-void  EXPORT PausePlayer (short refnum);
+void  EXPORT StartPlayer(short refnum);
+void  EXPORT ContPlayer(short refnum);
+void  EXPORT StopPlayer(short refnum);
+void  EXPORT PausePlayer(short refnum);
 
 // Record management
 
-void  EXPORT SetRecordModePlayer (short refnum, short state);
-void  EXPORT RecordPlayer (short refnum, short tracknum);
-void  EXPORT SetRecordFilterPlayer(short refnum, FilterPtr filter);
+void  EXPORT SetRecordModePlayer(short refnum, short state);
+void  EXPORT RecordPlayer(short refnum, short tracknum);
+void  EXPORT SetRecordFilterPlayer(short refnum, MidiFilterPtr filter);
 
 // Position management
 
-void  EXPORT SetPosBBUPlayer (short refnum, PosPtr pos);
-void  EXPORT SetPosMsPlayer (short refnum, long date_ms);
+void  EXPORT SetPosBBUPlayer(short refnum, PosPtr pos);
+void  EXPORT SetPosMsPlayer(short refnum, long date_ms);
+void  EXPORT SetPosTicksPlayer(short refnum, long date_ticks);
 
 // Loop management
 
-void  EXPORT SetLoopPlayer (short refnum, short state);
-long  EXPORT SetLoopStartBBUPlayer (short refnum,  PosPtr pos);
-long  EXPORT SetLoopEndBBUPlayer (short refnum,  PosPtr pos);
-long  EXPORT SetLoopStartMsPlayer(short refnum,   long date_ms);
-long  EXPORT SetLoopEndMsPlayer (short refnum,   long date_ms);
+void  EXPORT SetLoopPlayer(short refnum, short state);
+long  EXPORT SetLoopStartBBUPlayer(short refnum, PosPtr pos);
+long  EXPORT SetLoopEndBBUPlayer(short refnum, PosPtr pos);
+long  EXPORT SetLoopStartMsPlayer(short refnum, long date_ms);
+long  EXPORT SetLoopEndMsPlayer(short refnum, long date_ms);
 
 // Synchronisation management
 
-void  EXPORT SetSynchroInPlayer (short refnum, short state);
-void  EXPORT SetSynchroOutPlayer (short refnum,short state);
-void  EXPORT SetSMPTEOffsetPlayer (short refnum,SmpteLocPtr smptepos) ;
-void  EXPORT SetTempoPlayer (short refnum,long tempo);
+void  EXPORT SetSynchroInPlayer(short refnum, short state);
+void  EXPORT SetSynchroOutPlayer(short refnum, short state);
+void  EXPORT SetSMPTEOffsetPlayer(short refnum, SmpteLocPtr smptepos);
+void  EXPORT SetTempoPlayer(short refnum, long tempo);
 
 // State management
 
-void  EXPORT GetStatePlayer (short refnum, PlayerStatePtr playerstate) ;
-void  EXPORT GetEndScorePlayer (short refnum, PlayerStatePtr playerstate);
+void  EXPORT GetStatePlayer(short refnum, PlayerStatePtr playerstate);
+void  EXPORT GetEndScorePlayer(short refnum, PlayerStatePtr playerstate);
 
 // Step playing 
 
-void  EXPORT ForwardStepPlayer (short refnum , short flag);
-void  EXPORT BackwardStepPlayer (short refnum , short flag);
+void  EXPORT ForwardStepPlayer(short refnum, short flag);
+void  EXPORT BackwardStepPlayer(short refnum, short flag);
 
 // Tracks management
 
-MidiSeqPtr  EXPORT GetAllTrackPlayer (short refnum);
-MidiSeqPtr  EXPORT GetTrackPlayer (short refnum, short tracknum);
+MidiSeqPtr  EXPORT GetAllTrackPlayer(short refnum);
+MidiSeqPtr  EXPORT GetTrackPlayer(short refnum, short tracknum);
 
-long  EXPORT SetTrackPlayer (short refnum, short tracknum, MidiSeqPtr s);
-long  EXPORT SetAllTrackPlayer (short refnum, MidiSeqPtr s, long ticks_per_quarter);
+long  EXPORT SetTrackPlayer(short refnum, short tracknum, MidiSeqPtr s);
+long  EXPORT SetAllTrackPlayer(short refnum, MidiSeqPtr s, long ticks_per_quarter);
 
-void  EXPORT SetParamPlayer (short refnum, short tracknum, short param , short value);
-short EXPORT GetParamPlayer (short refnum, short tracknum, short  param);
+void  EXPORT SetParamPlayer(short refnum, short tracknum, short param , short value);
+short EXPORT GetParamPlayer(short refnum, short tracknum, short  param);
 
-long EXPORT InsertAllTrackPlayer(short refnum,MidiSeqPtr s);
+long EXPORT InsertAllTrackPlayer(short refnum, MidiSeqPtr s);
 long EXPORT InsertTrackPlayer(short refnum, short tracknum, MidiSeqPtr s);
 
-void EXPORT ApplyAllTrackPlayer(short refnum,  ApplyScorePtr fun, void* data);
-void EXPORT ApplyTrackPlayer(short refnum, short tracknum, ApplyScorePtr fun,void* data);
+void EXPORT ApplyAllTrackPlayer(short refnum, ApplyScorePtr fun, void* data);
+void EXPORT ApplyTrackPlayer(short refnum, short tracknum, ApplyScorePtr fun, void* data);
 
 
 #ifdef __Macintosh__
@@ -113,23 +124,24 @@ void EXPORT ApplyTrackPlayer(short refnum, short tracknum, ApplyScorePtr fun,voi
 // Can be called at interrupt level
 /*--------------------------------------------------------------------------*/
 
-short  EXPORT Version ()  {return kVersion;}
+short  EXPORT Version()  {return kVersion;}
 
 // Player structure management
 
 /*--------------------------------------------------------------------------*/
 
-short  EXPORT OpenPlayer (MidiName name )
+short  EXPORT OpenPlayer(MidiName name)
 {
-	TPlayerPtr player = new TPlayer();
+	TPlayer::Init();
+  	TPlayerPtr player = new TPlayer();
 	short res = player->Open(name);
- 	if (res < 0) delete player; 		// correspond a une erreur
+ 	if (res < 0) delete player; 		// when an error occurs
 	return res;
 }
 
 /*--------------------------------------------------------------------------*/
 
-void   EXPORT ClosePlayer (short refnum)
+void   EXPORT ClosePlayer(short refnum)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player){
@@ -142,7 +154,7 @@ void   EXPORT ClosePlayer (short refnum)
 // Transport control
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT StartPlayer (short refnum)
+void  EXPORT StartPlayer(short refnum)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->Start();
@@ -150,7 +162,7 @@ void  EXPORT StartPlayer (short refnum)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT ContPlayer (short refnum)
+void  EXPORT ContPlayer(short refnum)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->Cont();
@@ -158,7 +170,7 @@ void  EXPORT ContPlayer (short refnum)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT StopPlayer (short refnum)
+void  EXPORT StopPlayer(short refnum)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->Stop();
@@ -166,7 +178,7 @@ void  EXPORT StopPlayer (short refnum)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT PausePlayer (short refnum)
+void  EXPORT PausePlayer(short refnum)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->Pause();
@@ -176,7 +188,7 @@ void  EXPORT PausePlayer (short refnum)
 // Record management
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetRecordModePlayer (short refnum, short state)
+void  EXPORT SetRecordModePlayer(short refnum, short state)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->SetRecordMode(state);
@@ -201,7 +213,7 @@ void  EXPORT SetRecordFilterPlayer (short refnum, MidiFilterPtr filter)
 // Position management
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetPosBBUPlayer (short refnum, PosPtr pos)
+void  EXPORT SetPosBBUPlayer(short refnum, PosPtr pos)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->SetPosBBU(pos);
@@ -209,33 +221,41 @@ void  EXPORT SetPosBBUPlayer (short refnum, PosPtr pos)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetPosMsPlayer (short refnum, long date_ms)
+void  EXPORT SetPosMsPlayer(short refnum, long date_ms)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->SetPosMs(date_ms);
 }
 
 /*--------------------------------------------------------------------------*/
+
+void  EXPORT SetPosTicksPlayer(short refnum, long date_ticks)
+{
+	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
+	if (player) player->SetPosTicks(date_ticks);
+}
+
+/*--------------------------------------------------------------------------*/
 // Loop management
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetLoopPlayer (short refnum, short state)
+void  EXPORT SetLoopPlayer(short refnum, short state)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
-	if (player) player->SetLoop(state);
+	if (player) player->SetLoop((Boolean)state);
 }
 
 /*--------------------------------------------------------------------------*/
 
-long  EXPORT SetLoopStartBBUPlayer (short refnum,  PosPtr pos)
+long  EXPORT SetLoopStartBBUPlayer (short refnum, PosPtr pos)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
-	return (player) ? player->SetLoopStartBBU(pos) :kErrSequencer;
+	return (player) ? player->SetLoopStartBBU(pos) : kErrSequencer;
 }
 
 /*--------------------------------------------------------------------------*/
 
-long  EXPORT SetLoopEndBBUPlayer (short refnum,  PosPtr pos)
+long  EXPORT SetLoopEndBBUPlayer(short refnum, PosPtr pos)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player)? player->SetLoopEndBBU(pos):kErrSequencer;
@@ -243,7 +263,7 @@ long  EXPORT SetLoopEndBBUPlayer (short refnum,  PosPtr pos)
 
 /*--------------------------------------------------------------------------*/
 
-long  EXPORT SetLoopStartMsPlayer(short refnum,  long date_ms)
+long  EXPORT SetLoopStartMsPlayer(short refnum, long date_ms)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->SetLoopStartMs(date_ms): kErrSequencer;
@@ -251,7 +271,7 @@ long  EXPORT SetLoopStartMsPlayer(short refnum,  long date_ms)
 
 /*--------------------------------------------------------------------------*/
 
-long  EXPORT SetLoopEndMsPlayer (short refnum,   long date_ms)
+long  EXPORT SetLoopEndMsPlayer(short refnum, long date_ms)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->SetLoopEndMs(date_ms) : kErrSequencer;
@@ -261,7 +281,7 @@ long  EXPORT SetLoopEndMsPlayer (short refnum,   long date_ms)
 // Synchronisation management
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetSynchroInPlayer (short refnum, short state)
+void  EXPORT SetSynchroInPlayer(short refnum, short state)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->SetSynchroIn(state);
@@ -269,7 +289,7 @@ void  EXPORT SetSynchroInPlayer (short refnum, short state)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetSynchroOutPlayer (short refnum,short state)
+void  EXPORT SetSynchroOutPlayer(short refnum, short state)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->SetSynchroOut(state);
@@ -277,7 +297,7 @@ void  EXPORT SetSynchroOutPlayer (short refnum,short state)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetTempoPlayer (short refnum,long tempo)
+void  EXPORT SetTempoPlayer(short refnum, long tempo)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->SetTempo(tempo);
@@ -285,7 +305,7 @@ void  EXPORT SetTempoPlayer (short refnum,long tempo)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetSMPTEOffsetPlayer (short refnum, SmpteLocPtr smptepos)
+void  EXPORT SetSMPTEOffsetPlayer(short refnum, SmpteLocPtr smptepos)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->SetSMPTEOffset(smptepos);
@@ -294,7 +314,7 @@ void  EXPORT SetSMPTEOffsetPlayer (short refnum, SmpteLocPtr smptepos)
 // State management
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT GetStatePlayer (short refnum, PlayerStatePtr playerstate)
+void  EXPORT GetStatePlayer(short refnum, PlayerStatePtr playerstate)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->GetState(playerstate);
@@ -302,7 +322,7 @@ void  EXPORT GetStatePlayer (short refnum, PlayerStatePtr playerstate)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT GetEndScorePlayer (short refnum, PlayerStatePtr playerstate)
+void  EXPORT GetEndScorePlayer(short refnum, PlayerStatePtr playerstate)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->GetEndScore(playerstate);
@@ -312,7 +332,7 @@ void  EXPORT GetEndScorePlayer (short refnum, PlayerStatePtr playerstate)
 // Step playing 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT ForwardStepPlayer (short refnum , short flag)
+void  EXPORT ForwardStepPlayer(short refnum, short flag)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->ForwardStep(flag);
@@ -320,7 +340,7 @@ void  EXPORT ForwardStepPlayer (short refnum , short flag)
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT BackwardStepPlayer (short refnum , short flag)
+void  EXPORT BackwardStepPlayer(short refnum, short flag)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->BackwardStep(flag);
@@ -330,7 +350,7 @@ void  EXPORT BackwardStepPlayer (short refnum , short flag)
 // Tracks management
 /*--------------------------------------------------------------------------*/
 
-MidiSeqPtr  EXPORT GetAllTrackPlayer (short refnum)
+MidiSeqPtr  EXPORT GetAllTrackPlayer(short refnum)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->GetAllTrack() : 0;
@@ -338,7 +358,7 @@ MidiSeqPtr  EXPORT GetAllTrackPlayer (short refnum)
 
 /*--------------------------------------------------------------------------*/
 
-MidiSeqPtr  EXPORT GetTrackPlayer (short refnum, short tracknum)
+MidiSeqPtr  EXPORT GetTrackPlayer(short refnum, short tracknum)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->GetTrack(tracknum) : 0;
@@ -346,7 +366,7 @@ MidiSeqPtr  EXPORT GetTrackPlayer (short refnum, short tracknum)
 
 /*--------------------------------------------------------------------------*/
 
-long  EXPORT SetTrackPlayer (short refnum, short tracknum, MidiSeqPtr s)
+long  EXPORT SetTrackPlayer(short refnum, short tracknum, MidiSeqPtr s)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->SetTrack(tracknum,s) :kErrSequencer;
@@ -354,7 +374,7 @@ long  EXPORT SetTrackPlayer (short refnum, short tracknum, MidiSeqPtr s)
 
 /*--------------------------------------------------------------------------*/
 
-long  EXPORT SetAllTrackPlayer (short refnum, MidiSeqPtr s, long ticks_per_quarter)
+long  EXPORT SetAllTrackPlayer(short refnum, MidiSeqPtr s, long ticks_per_quarter)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->SetAllTrack(s,ticks_per_quarter) :kErrSequencer;
@@ -362,7 +382,7 @@ long  EXPORT SetAllTrackPlayer (short refnum, MidiSeqPtr s, long ticks_per_quart
 
 /*--------------------------------------------------------------------------*/
 
-void  EXPORT SetParamPlayer (short refnum, short tracknum, short param , short value)
+void  EXPORT SetParamPlayer(short refnum, short tracknum, short param, short value)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	if (player) player->SetParam(tracknum,param,value);
@@ -370,7 +390,7 @@ void  EXPORT SetParamPlayer (short refnum, short tracknum, short param , short v
 
 /*--------------------------------------------------------------------------*/
 
-short  EXPORT GetParamPlayer (short refnum, short tracknum, short  param)
+short  EXPORT GetParamPlayer(short refnum, short tracknum, short param)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->GetParam(tracknum,param) : kErrSequencer;
@@ -378,7 +398,7 @@ short  EXPORT GetParamPlayer (short refnum, short tracknum, short  param)
 
 /*--------------------------------------------------------------------------*/
 
-long  EXPORT InsertAllTrackPlayer (short refnum,  MidiSeqPtr  s)
+long  EXPORT InsertAllTrackPlayer(short refnum, MidiSeqPtr s)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->InsertAllTrack(s) : kErrSequencer;
@@ -386,7 +406,7 @@ long  EXPORT InsertAllTrackPlayer (short refnum,  MidiSeqPtr  s)
 
 /*--------------------------------------------------------------------------*/
 
-long  EXPORT InsertTrackPlayer (short refnum, short tracknum, MidiSeqPtr  s)
+long  EXPORT InsertTrackPlayer(short refnum, short tracknum, MidiSeqPtr s)
 {
 	TPlayerPtr player = (TPlayerPtr)MidiGetInfo(refnum);
 	return (player) ? player->InsertTrack(tracknum,s) : kErrSequencer;
