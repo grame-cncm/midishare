0953ea3f51457a681366a0673876890d50aee770
dfober <dfober>
2005-06-28 10:32:54 +0000

    platform declarations refreshed

--- libraries/midifile/midifile.h
+++ libraries/midifile/midifile.h
@@ -1,60 +1,58 @@
-/* ===========================================================================
-   This file is Copyright (c) Grame, Computer Music Research Laboratory 
-   1996-1999, and is distributed as Open Source software under the Artistic License;
-   see the file "Artistic" that is included in the distribution for details.
+/*
 
-   Grame : Computer Music Research Laboratory
-   Web : http://www.grame.fr/Research
-   E-mail : MidiShare@rd.grame.fr
-   ===========================================================================
- *	MIDIFILE.H
- *	MIDI Files	version 1.00
- */
+  Copyright © Grame 2004
+
+  This library is free software; you can redistribute it and modify it under 
+  the terms of the GNU Library General Public License as published by the 
+  Free Software Foundation version 2 of the License, or any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License 
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Grame Research Laboratory, 9, rue du Garet 69001 Lyon - France
+  research@grame.fr
+
+*/
  
  
 #ifndef __MIDIFILE_H__
 #define __MIDIFILE_H__
 
 #include <stdio.h>
-
-#ifdef __Linux__
-#define FAR
-#define NEAR
-#define MFAPI
 #include "MidiShare.h"
+
 #define nil 0
-#define errno  /* a revoir */
-#endif
 
+#if defined(linux) || defined(__MACH__)
+# define FAR
+# define NEAR
+# define MFAPI
+# define errno  /* a revoir */
 
-#ifdef __Macintosh__
-#include <midisharePPC.h>
-#define FAR
-#define NEAR
-#define MFAPI
-#endif
+#elif defined(mac_classic)
+# include <midisharePPC.h>
+# define FAR
+# define NEAR
+# define MFAPI
 
-#ifdef __Windows__
-#include <mshare.h>
-#define nil 0
-#ifdef WIN32
+#elif defined(WIN32)
+# ifdef __BuildLib__
+#	define MFAPI	__declspec(dllexport)
+# else
+#	define MFAPI	__declspec(dllimport)
+# endif
 
-#	ifdef __BuildLib__
-
-#		define MFAPI	__declspec(dllexport)
-
-#	else
-
-#		define MFAPI	__declspec(dllimport)
-
-#	endif
+#elif defined(WIN16)
+# define MFAPI WINAPI _export
 
 #else
-
-#   define MFAPI WINAPI _export
-
-#endif
-
+#error "architecture undefined"
 #endif
 
 
