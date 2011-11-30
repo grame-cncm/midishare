34aab561991ee7f13716bd87865aa97f8289a3d9
D. Fober <fober@grame.fr>
2011-11-30 14:51:58 +0100

    test java

--- lang/java/native/Test.java
+++ lang/java/native/Test.java
@@ -16,11 +16,55 @@ public class Test {
 
    public static void basictest() {
 		int ref = Midi.Open("test");
+		int ref2 = Midi.Open("rcv");
 		System.out.println(Midi.GetName(ref) + " application ref: " + ref);
 		System.out.println("Current time is: " + Midi.GetTime());
+		System.out.println("Free space: " + Midi.FreeSpace());
+		Midi.Connect (ref, 0, 1);
+		Midi.Connect (ref, ref2, 1);
+		System.out.println("Connection state: " + Midi.IsConnected(1,0));
+		sendNote (72, ref);
 		while (Midi.GetTime() < 2000) ;
+		
+		long ev = Midi.GetEv (ref2);
+		System.out.println("rcv fifo ev: " + ev);
+		if (ev != 0) {
+			printev (ev);
+			Midi.FreeEv (ev);
+		}
+		ev = Midi.GetEv (ref2);
+		System.out.println("rcv fifo ev: " + ev);
+		if (ev != 0) Midi.FreeEv (ev);
+		
 		System.out.println("Time to exit: " + Midi.GetTime());
-		Midi.Close(ref);		
+		Midi.Connect (ref, ref2, 0);
+		Midi.Close(ref2);		
+		Midi.Close(ref);
    }
 
+	static void printev(long e) 
+	{
+		System.out.println("Event " + e + ":");
+		System.out.println("  date   " + Midi.GetDate(e));
+		System.out.println("  refnum " + Midi.GetRefnum(e));
+		System.out.println("  type   " + Midi.GetType(e));
+		System.out.println("  chan   " + Midi.GetChan(e));
+		System.out.println("  port   " + Midi.GetPort(e));
+		System.out.println("  pitch  " + Midi.GetField(e, 0));
+		System.out.println("  vel    " + Midi.GetField(e, 1));
+		System.out.println("  dur    " + Midi.GetField(e, 2));
+	}
+
+	static void sendNote(int pitch, int ref) 
+	{
+		long event = Midi.NewEv(Midi.typeNote);  // ask for a new note event	
+		if (event != 0) {					// if the allocation was succesfull
+			Midi.SetChan(event,0); 			// set the Midi channel
+			Midi.SetPort(event,0); 			// set the destination port
+			Midi.SetField(event,0,pitch); 	// set the pitch field
+			Midi.SetField(event,1,80); 		// set the velocity field
+			Midi.SetField(event,2,1000); 	// set the duration field
+			Midi.SendIm(ref, event);  		// send the note immediatly
+		}
+	}
 }
