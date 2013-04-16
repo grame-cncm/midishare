c4b65582e25d5630496e54e4654f8165962d5da4
Dominique Fober <fober@grame.fr>
2013-04-16 13:07:40 +0200

    license changed to Mozilla Public License

--- midisharelight/src/MidiShareLight.cpp
+++ midisharelight/src/MidiShareLight.cpp
@@ -1,4 +1,14 @@
+/*
+  MidiShare Project
+  Copyright (C) Grame 1999-2011
 
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+*/
 
 #include "MidiShareLight.h"
 #include "msMemory.h"
